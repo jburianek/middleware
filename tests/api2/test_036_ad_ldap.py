d54960c224389cf2a13361a044a2ef5f8a76e735
Andrew Walker <awalker@ixsystems.com>
2023-01-27 10:09:14 -0800

    Set NFS v4

--- tests/api2/test_036_ad_ldap.py
+++ tests/api2/test_036_ad_ldap.py
@@ -53,7 +53,7 @@ test_flags = {
 
 @pytest.fixture(scope="module")
 def kerberos_config(request):
-    results = PUT("/nfs/", {"v4_krb": True})
+    results = PUT("/nfs/", {"v4": True, "v4_krb": True})
     assert results.status_code == 200, results.text
     try:
         yield (request, results.json())
